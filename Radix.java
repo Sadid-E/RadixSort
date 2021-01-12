8a63bf76ef78cd02d157364209073240153c59bd
Sadid-E <sethun20@stuy.edu>
2021-01-12 13:12:29 -0500

    Added the length method

--- Radix.java
+++ Radix.java
@@ -5,4 +5,13 @@ public class Radix {
     return Integer.parseInt(number.substring(number.length()-col-1, number.length()-col));
   }
 
+  public static int length(int n) {
+    String number = "" + n;
+    int size = number.length();
+    if (number.charAt(0) == '-') {
+      size -= 1;
+    }
+    return size;
+  }
+
 }
