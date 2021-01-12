91c211b9819b5ec2c9f29cc5ecc2d142a498ceea
Sadid-E <sethun20@stuy.edu>
2021-01-12 13:03:20 -0500

    Added the nth method

--- Radix.java
+++ Radix.java
@@ -1,3 +1,8 @@
-public class RadixSort {
+public class Radix {
+
+  public static int nth(int n, int col) {
+    String number = "" + n;
+    return Integer.parseInt(number.substring(number.length()-col-1, number.length()-col));
+  }
 
 }
