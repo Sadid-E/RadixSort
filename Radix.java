ad18dd12de767c2ab416a4df3c73211623f49bed
Sadid-E <sethun20@stuy.edu>
2021-01-13 00:23:07 -0500

    Added the radixSortSimple method

--- Radix.java
+++ Radix.java
@@ -2,6 +2,9 @@ public class Radix {
 
   public static int nth(int n, int col) {
     String number = "" + n;
+    if (col >= number.length()) {
+      return 0;
+    }
     return Integer.parseInt(number.substring(number.length()-col-1, number.length()-col));
   }
 
@@ -15,8 +18,29 @@ public class Radix {
   }
 
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
-    for (int i = 0; i <= 10; i++) {
+    for (int i = 0; i < 10; i++) {
       original.extend(buckets[i]);
+    }
+  }
+
+  public static void radixSortSimple(SortableLinkedList data) {
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i++) {
+      SortableLinkedList bucket = new SortableLinkedList();
+      buckets[i] = bucket;
+    }
+    int times = 1;
+    int size = data.size();
+    for (int j = 0; j < times; j++) {
+      for (int k = 0; k < size; k++) {
+        int num = data.remove(0);
+        int digit = nth(num,j);
+        buckets[digit].add(num);
+        if (length(num) > times) {
+          times = length(num);
+        }
+      }
+      merge(data,buckets);
     }
   }
 
