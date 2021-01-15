898d02c67ff05a85cc38e8f9d1ca7bf0fda65615
Sadid-E <sethun20@stuy.edu>
2021-01-15 01:03:46 -0500

    Finished radixSort

--- Radix.java
+++ Radix.java
@@ -1,14 +1,14 @@
 public class Radix {
 
   public static int nth(int n, int col) {
-    return (int) (n/(Math.pow(10,col)) % 10);
+    return (int) (Math.abs(n)/(Math.pow(10,col)) % 10);
   }
 
   public static int length(int n) {
     if (n == 0) {
       return 1;
     }
-    return (int) (Math.log10(n) + 1);
+    return (int) (Math.log10(Math.abs(n)) + 1);
   }
 
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
@@ -39,25 +39,32 @@ public class Radix {
   }
 
   public static void radixSort(SortableLinkedList data) {
-    // SortableLinkedList negatives = new SortableLinkedList();
-    // int size = data.size();
-    // for (int i = 0; i < size; i++) {
-    //   int num = data.remove(0);
-    //   if (num < 0) {
-    //     negatives.add(Math.abs(num));
-    //   } else {
-    //     data.add(num);
-    //   }
-    // }
-    // radixSortSimple(data);
-    // radixSortSimple(negatives);
-    // int size1 = negatives.size();
-    // for (int j = 0; j < size1; j++) {
-    //   int num = negatives.remove(0);
-    //   negatives.add(0-num);
-    // }
-    // negatives.extend(data);
-    // data.extend(negatives);
+    SortableLinkedList[] bucketsNegative = new SortableLinkedList[10];
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i++) {
+      SortableLinkedList bucket = new SortableLinkedList();
+      SortableLinkedList bucket2 = new SortableLinkedList();
+      buckets[i] = bucket;
+      bucketsNegative[i] = bucket2;
+    }
+    int times = 1;
+    int size = data.size();
+    for (int j = 0; j < times; j++) {
+      for (int k = 0; k < size; k++) {
+        int num = data.remove(0);
+        int digit = nth(num,j);
+          if (num < 0) {
+            bucketsNegative[9-Math.abs(digit)].add(num);
+          } else {
+            buckets[digit].add(num);
+          }
+          if (length(num) > times) {
+            times = length(num);
+          }
+      }
+      merge(data,bucketsNegative);
+      merge(data,buckets);
+    }
   }
 
 }
